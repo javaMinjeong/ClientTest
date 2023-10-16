51bc15166d658ac9e8c290b7c81a31644c0d202f
javaMinjeong <mmjjkk01234@naver.com>
2023-10-17 08:47:32 +0900

    23.10.16수업

---
+++ src/main/webapp/asset/js/scroll.js
@@ -0,0 +1,20 @@
+//scroll.js
+
+
+// 1. <div id='bar'></div> 동적 추가
+// 2. CSS 적용
+//3. 이벤트 추가
+
+$('<div id="scroll.bar-indicator"></div>').css({
+    width: 0,
+    height: '5px',
+    backgroundColor: 'cornflowerblue',
+    position: 'fixed',
+    left: 0,
+    top: 0
+}).prependTo($('body'));
+
+$(document).scroll(function(){
+    let x = $(document).scrollTop()*100 / $(document).outerHeight() - $(window).outerHeight();
+    $('#scroll-bar-indicator').css('width', x + '%');
+});
